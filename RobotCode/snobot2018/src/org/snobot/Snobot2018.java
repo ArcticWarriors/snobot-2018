588adffda9d18de93bf589c0d922c2119261f6ae
PJ Reiniger <pj.reiniger@gmail.com>
2017-12-16 18:48:56 -0500

    Initial commit of boilerplate code

---
+++ RobotCode/snobot2018/src/org/snobot/Snobot2018.java
@@ -0,0 +1,15 @@
+package org.snobot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Snobot2018 extends IterativeRobot {
+
+}
+
---
+++ generate_ide_files.bat
@@ -0,0 +1,11 @@
+
+cd RobotCode/snobot2018
+call gradlew ide
+call gradlew eclipse
+cd ../..
+
+
+cd NetworkTableViewer/2018NetworkTableViewer
+call gradlew ide
+call gradlew eclipse
+cd ../..
