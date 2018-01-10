11cb60c45b506d2b1d0114c0626046d4afc01072
PJ Reiniger <pj.reiniger@gmail.com>
2018-01-10 00:21:19 -0500

    Updating build script to point to latest simulator

--- RobotCode/snobot2018/src/org/snobot/Snobot2018.java
+++ RobotCode/snobot2018/src/org/snobot/Snobot2018.java
@@ -1,15 +1,17 @@
 package org.snobot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import com.snobot.lib.ASnobot;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
-public class Snobot2018 extends IterativeRobot {
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class Snobot2018 extends ASnobot
+{
+
+    @Override
+    protected CommandGroup createAutonomousCommand()
+    {
+        return null;
+    }
 
 }
 
