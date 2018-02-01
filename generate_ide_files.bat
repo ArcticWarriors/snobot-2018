a364847dc783be6de0eb0bf1099aa62ffcbd9b26
Jeffrey Noll <jbnoll4060@gmail.com>
2018-02-01 18:20:00 -0500

    Trajectory path (#7)
    * Trajectory stuff
    
    * Updating trajectory logic
    
    - Added some trajectores
    - Removed the need for a local generator script.  All done from the
    standalone coordinate gui
    
    * Removing unnecessary source directory

--- generate_ide_files.bat
+++ generate_ide_files.bat
@@ -1,17 +1,26 @@
 
 cd RobotCode/snobot2018
-call gradlew ide
+call gradlew cleanIde
 call gradlew eclipse
 cd ../..
 
 
 cd NetworkTableViewer/2018SmartDashboardWidgets
-call gradlew ide
+call gradlew cleanIde
 call gradlew eclipse
 cd ../..
 
 
 cd NetworkTableViewer/2018ShuffleboardWidgets
-call gradlew ide
+call gradlew cleanIde
 call gradlew eclipse
+cd ../..
+
+:: Legacy, shouldn't be there anymore
+cd NetworkTableViewer/2018NetworkTableViewer
+DEL .classpath
+DEL .project
+DEL 2018NetworkTableViewer.iml
+DEL 2018NetworkTableViewer.ipr
+DEL 2018NetworkTableViewer.iws
 cd ../..
